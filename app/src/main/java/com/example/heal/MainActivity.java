693181b900961afcc15e4f4922c265cd682e23ec
Ali <153304452+SyedAliMurtaza1@users.noreply.github.com>
2026-04-24 15:03:35 +0500

    front screen

--- app/src/main/java/com/example/heal/MainActivity.java
+++ app/src/main/java/com/example/heal/MainActivity.java
@@ -1,24 +1,40 @@
 package com.example.heal;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button btnGetStarted = findViewById(R.id.btnGetStarted);
+        TextView tvLogin = findViewById(R.id.tvLogin);
+
+        btnGetStarted.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Get Started clicked!", Toast.LENGTH_SHORT).show();
+                Intent I = new Intent(MainActivity.this, Signup.class);
+                startActivity(I);
+            }
+        });
+
+        tvLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Login clicked!", Toast.LENGTH_SHORT).show();
+                Intent I = new Intent(MainActivity.this, Login.class);
+                startActivity(I);
+            }
         });
     }
 }
